8922dbe74f51e86dd04dc79d4ee60bf41de7ad78
Cyril Bonaccini <cyrilb@bam.tech>
2022-06-30 23:36:59 +0200

    chore(lint): setup eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { applyPlugin } from "./plugins/engine/plugin.engine.js";
-import { eslintPlugin } from "./plugins/eslint/estlint-v1.plugin.js";
+import { applyPlugin } from './plugins/engine/plugin.engine.js';
+import { eslintPlugin } from './plugins/eslint/estlint-v1.plugin.js';
 
 applyPlugin(eslintPlugin);
--- src/plugins/eslint/estlint-v1.plugin.ts
+++ src/plugins/eslint/estlint-v1.plugin.ts
@@ -1,24 +1,24 @@
 // import { $ } from "zx";
-import { Plugin } from "../Plugin";
-import { Step } from "../Step";
+import { Plugin } from '../Plugin';
+import { Step } from '../Step';
 
 const installStep: Step = {
   run: async () => {
-    console.log("install step");
+    console.log('install step');
     // $`yarn add eslint@^8.18.0 --dev`;
   },
 };
 
 const applyPatchStep: Step = {
   run: async () => {
-    console.log("Apply patch step");
+    console.log('Apply patch step');
     // $`yarn add eslint@^8.18.0 --dev`;
   },
 };
 
 export const eslintPlugin = new Plugin({
-  id: "estlint-v1",
-  displayName: "ESLint v1",
-  version: "1",
+  id: 'estlint-v1',
+  displayName: 'ESLint v1',
+  version: '1',
   steps: [installStep, applyPatchStep],
 });
