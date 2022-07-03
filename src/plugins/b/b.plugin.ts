9cb4f11fc36fcf7be17f2baefaec9e3202dfdf9d
Cyril Bonaccini <cyrilb@bam.tech>
2022-07-03 22:57:07 +0200

    feat(plugin): update a and b plugin examples

--- src/plugins/b/b.plugin.ts
+++ src/plugins/b/b.plugin.ts
@@ -1,20 +1,38 @@
-import { $ } from 'zx';
 import { Plugin } from '../../engine/Plugin/Plugin.type';
 import { Step } from '../../engine/Plugin/Step.type';
 
 const firstStep: Step = {
   name: 'first step of plugin b',
-  commitMessage: 'chore(b): doing something else',
+  commitMessage: 'chore(b): doing something',
+  failureProcedure: ['run the command aaa', 'run the command bbb'],
   run: async () => {
-    const random = Math.floor(Math.random() * 1000);
-    await $`touch tmp/${random}.txt`;
-    await $`echo "${random}" > tmp/${random}.txt`;
+    console.log('first step run successfully');
   },
 };
+
+const secondStep: Step = {
+  name: 'second step of plugin b',
+  commitMessage: 'chore(b): doing something else',
+  failureProcedure: ['run the command aaa', 'run the command bbb'],
+  run: async () => {
+    console.log('Oops, something really bad happened');
+    throw new Error('Something ba happened');
+  },
+};
+
+const thirdStep: Step = {
+  name: 'third step of plugin b',
+  commitMessage: 'chore(b): doing nothing',
+  failureProcedure: ['run the command aaa', 'run the command bbb'],
+  run: async () => {
+    console.log('third step run successfully');
+  },
+};
+
 export const bPlugin: Plugin = {
   id: 'plugin-b',
   displayName: 'Plugin b',
   version: '1',
-  steps: [firstStep],
+  steps: [firstStep, secondStep, thirdStep],
   dependencies: [],
 };
